9a3731a2532364d0ca2a7aa1f5391015e976ea21
frcai <frcai@microsoft.com>
2022-08-02 17:10:45 +0800

    Update export-service.sh (#385)
    * Update export-service.sh
    
    * Update export-service.sh

--- previews/inferencing-migration/export-service.sh
+++ previews/inferencing-migration/export-service.sh
@@ -35,3 +35,7 @@ echo "Params have been saved to $params"
 echo "Deploy $migrate_type service $online_endpoint_name..."
 deployment_name="Migration-$online_endpoint_name-$(echo $RANDOM | md5sum | head -c 4)"
 az deployment group create --name "$deployment_name" --resource-group "$resource_group" --template-file "$template_file" --parameters "$params" --subscription $subscription_id
+
+# STEP5 Clean up exported files in blob storage
+echo 'Clean up exported files in blob storage...'
+az storage blob directory delete -c azureml --account-name "$storage_account" -d "$blob_folder" --recursive --subscription $subscription_id --only-show-errors 1> /dev/null
